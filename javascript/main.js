ecdf66bb529956918d3c620ddaea4b019c19836f
darkp0lx <polbarazorda@gmail.com>
2020-04-26 20:07:06 -0500

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,27 @@
+let item=0
+let left=document.querySelector(".fa-arrow-left")
+let right=document.querySelector(".fa-arrow-right")
+left.addEventListener("click",()=>{changeItem(1)})
+right.addEventListener("click",()=>{changeItem(2)})
+function changeItem(n){
+    if(n==1){
+        if(item==0){
+            item=4
+        }
+        else{
+            item--
+        }
+    }else{
+        if(item==4){
+            item=0
+        }else{
+            item++
+        }
+    }
+    for (let i = 0; i < document.querySelectorAll(".item").length; i++) {
+        document.querySelectorAll(".item")[i].style.display="none"
+        
+    }
+    document.querySelectorAll(".item")[item].style.display="flex"
+
+}
